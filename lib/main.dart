d187b2929547e63f69edcc4644745b1e58aeefc5
albaraa zain <albaraazain33@gmail.com>
2025-02-17 11:49:54 +0300

    feat: Remove Firebase initialization and logging from main.dart for cleaner startup

--- lib/main.dart
+++ lib/main.dart
@@ -12,37 +12,7 @@ import 'dart:developer' as developer;
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  developer.log('Initializing Firebase...');
-
-  try {
-    // Initialize Firebase with options
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-    developer.log('Firebase initialized successfully');
-
-    // Set up auth state listener
-    FirebaseAuth.instance.authStateChanges().listen((User? user) {
-      if (user != null) {
-        developer.log('User is signed in: ${user.uid}');
-      } else {
-        developer.log('User is signed out');
-      }
-    });
-
-    // Test Firestore connection
-    try {
-      await FirebaseFirestore.instance.collection('test').get();
-      developer.log('Successfully tested Firestore connection');
-    } catch (e) {
-      developer.log('Error testing Firestore connection', error: e);
-    }
-
-  } catch (e, stack) {
-    developer.log('Error initializing Firebase',
-        error: e, stackTrace: stack);
-    // We'll still run the app, but it might not work properly
-  }
+  developer.log('Starting app in development mode...');
 
   runApp(
     ProviderScope(
