3e16f229fb80cfd9c1893b2f6829bd5b1ab1667e
albaraa zain <albaraazain33@gmail.com>
2025-02-17 10:08:10 +0300

    feat: Integrate Firebase configuration in iOS AppDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,14 +1,14 @@
-import Flutter
 import UIKit
-import GoogleMaps
+import Flutter
+import FirebaseCore
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyCJ72tAQidcebxcF4AxBWoqTGf3GUNbEAo")
+    FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
